45314127c20e5dc13401b63a4fca2abf9c309658
Muhammad <sohailkhatak6566@gmail.com>
2025-05-31 00:29:57 -0700

    Notes_Keepe With Model and Notes_view_model

--- lib/views/notes_details_view.dart
+++ lib/views/notes_details_view.dart
@@ -40,129 +40,132 @@ class _NotesDetailsViewState extends State<NotesDetailsView> {
       appBar: AppBar(
         title: Text(args.appBarTitle),
       ),
-      body: Column(
-        children: [
-          ListTile(
-              title: DropdownButton(
-                  items: noteProvider.priorityList.map((String dropDownItem) {
-                    return DropdownMenuItem<String>(
-                        value: dropDownItem, child: Text(dropDownItem));
-                  }).toList(),
-                  iconEnabledColor: Colors.indigo,
-                  iconDisabledColor: Colors.white,
-                  iconSize: 35,
-                  value: noteProvider.priorityAsString(noteProvider.priority),
-                  style: TextStyle(
-                      color: Colors.indigo,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold),
-                  onChanged: (selectedPriority) {
-                    noteProvider.priorityAsInt(selectedPriority!);
-                  })),
-          SizedBox(
-            height: 10,
-          ),
-
-          ///Title TextField
-          TextField(
-            controller: _titleController,
-            decoration: InputDecoration(
-              label: Text("Title"),
-              labelStyle: TextStyle(color: Colors.indigo),
-              hintText: "Enter Your Title",
-              enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25),
-                  borderSide: BorderSide(color: Colors.indigo, width: 2)),
-              focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25),
-                  borderSide: BorderSide(color: Colors.indigo, width: 4)),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+                title: DropdownButton(
+                    items: noteProvider.priorityList.map((String dropDownItem) {
+                      return DropdownMenuItem<String>(
+                          value: dropDownItem, child: Text(dropDownItem));
+                    }).toList(),
+                    iconEnabledColor: Colors.indigo,
+                    iconDisabledColor: Colors.white,
+                    iconSize: 35,
+                    value: noteProvider.priorityAsString(noteProvider.priority),
+                    style: TextStyle(
+                        color: Colors.indigo,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                    onChanged: (selectedPriority) {
+                      noteProvider.priorityAsInt(selectedPriority!);
+                    })),
+            SizedBox(
+              height: 10,
             ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
 
-          ///Description TextField
-          TextField(
-            controller: _descriptionController,
-            decoration: InputDecoration(
-              label: Text("Description"),
-              labelStyle: TextStyle(color: Colors.indigo),
-              hintText: "Enter Your Description",
-              enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25),
-                  borderSide: BorderSide(color: Colors.indigo, width: 2)),
-              focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25),
-                  borderSide: BorderSide(color: Colors.indigo, width: 4)),
-            ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            ///SAVE Button
-            Container(
-              width: 150,
-              height: 50,
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: WidgetStatePropertyAll(Colors.indigo)),
-                  onPressed: () async {
-                    String time = DateFormat.yMMMd().format(DateTime.now());
-                    if (args.appBarTitle == "Add Note") {
-                      NoteModel noteModel = NoteModel(
-                          priority: noteProvider.priority,
-                          title: _titleController.text.trim(),
-                          description: _descriptionController.text.trim(),
-                          time: time);
-                      await noteProvider.insertData(noteModel);
-                    } else if (args.appBarTitle == "Edit Note") {
-                      String title = _titleController.text;
-                      String description = _descriptionController.text;
-                      NoteModel noteModel = NoteModel(
-                          id: args.noteModel!.id,
-                          priority: noteProvider.priority,
-                          title: title,
-                          description: description,
-                          time: time);
-                      await noteProvider.updateNote(noteModel);
-                    }
-                    Navigator.of(context).pop();
-                  },
-                  child: Text(
-                    args.buttonText,
-                    style: TextStyle(color: Colors.white),
-                  )),
+            ///Title TextField
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                label: Text("Title"),
+                labelStyle: TextStyle(color: Colors.indigo),
+                hintText: "Enter Your Title",
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    borderSide: BorderSide(color: Colors.indigo, width: 2)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    borderSide: BorderSide(color: Colors.indigo, width: 4)),
+              ),
             ),
             SizedBox(
-              width: 10,
+              height: 40,
             ),
 
-            ///Deleted Button
-            Container(
-              width: 150,
-              height: 50,
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: WidgetStatePropertyAll(Colors.indigo)),
-                  onPressed: () {
-                    if (args.appBarTitle == "Edit Note") {
-                      noteProvider.deleteNote(args.noteModel!.id!);
-                      Navigator.of(context).pop();
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Note is not deleted')));
-                    }
-                  },
-                  child: Text(
-                    "DELETE",
-                    style: TextStyle(color: Colors.white),
-                  )),
+            ///Description TextField
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(
+                label: Text("Description"),
+                labelStyle: TextStyle(color: Colors.indigo),
+                hintText: "Enter Your Description",
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    borderSide: BorderSide(color: Colors.indigo, width: 2)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    borderSide: BorderSide(color: Colors.indigo, width: 4)),
+              ),
             ),
-          ])
-        ],
+            SizedBox(
+              height: 40,
+            ),
+
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ///SAVE Button
+              Container(
+                width: 150,
+                height: 50,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Colors.indigo)),
+                    onPressed: () async {
+                      String time = DateFormat.yMMMd().format(DateTime.now());
+                      if (args.appBarTitle == "Add Note") {
+                        NoteModel noteModel = NoteModel(
+                            priority: noteProvider.priority,
+                            title: _titleController.text.trim(),
+                            description: _descriptionController.text.trim(),
+                            time: time);
+                        await noteProvider.insertData(noteModel);
+                      } else if (args.appBarTitle == "Edit Note") {
+                        String title = _titleController.text;
+                        String description = _descriptionController.text;
+                        NoteModel noteModel = NoteModel(
+                            id: args.noteModel!.id,
+                            priority: noteProvider.priority,
+                            title: title,
+                            description: description,
+                            time: time);
+                        await noteProvider.updateNote(noteModel);
+                      }
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      args.buttonText,
+                      style: TextStyle(color: Colors.white),
+                    )),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+
+              ///Deleted Button
+              Container(
+                width: 150,
+                height: 50,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Colors.indigo)),
+                    onPressed: () {
+                      if (args.appBarTitle == "Edit Note") {
+                        noteProvider.deleteNote(args.noteModel!.id!);
+                        Navigator.of(context).pop();
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Note is not deleted')));
+                      }
+                    },
+                    child: Text(
+                      "DELETE",
+                      style: TextStyle(color: Colors.white),
+                    )),
+              ),
+            ])
+          ],
+        ),
       ),
     );
   }
